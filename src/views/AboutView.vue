d842953b0a59000201163b6b9009fdc951eaad2d
skyler_chou <skyler_chou@donkeymove.com>
2023-06-27 17:59:37 +0800

    feat : 新增頁面背景

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="text-white p-40">About</div>
+  <div
+    class="h-full pt-16 pl-10 pr-10 sm:pt-44 sm:pl-20 bg-scroll bg-contain bg-center bg-no-repeat"
+    style="background-image: url('./src/assets/Bg-pc.png')"
+  >
+    <div class="text-white p-40">About</div>
+  </div>
 </template>
 
 <script>
 export default {
-  setup() {
-
-  },
+  setup() {},
 };
 </script>
--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="text-white p-40">Article</div>
+  <div
+    class="h-full pt-16 pl-10 pr-10 sm:pt-44 sm:pl-20 bg-scroll bg-contain bg-center bg-no-repeat"
+    style="background-image: url('./src/assets/Bg-pc.png')"
+  >
+    <div class="text-white p-40">Article</div>
+  </div>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
-
-<style>
-
-</style>
