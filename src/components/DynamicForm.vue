0841e2ba5416ca3883b6b836ee30378818bfc3cf
skyler_chou <skyler_chou@donkeymove.com>
2023-06-29 10:58:15 +0800

    feat : 試著使用 vee-validate 基本表單、動態表單

---
+++ src/components/DynamicForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <VForm>
+    <div
+      v-for="field in schema.fields" :key="field.name">
+      <label :for="field.name">{{ field.label }}</label>
+      <VField :as="field.as" :id="field.name" :name="field.name" />
+    </div>
+    <button>Submit</button>
+  </VForm>
+</template>
+<script>
+
+export default {
+  name: 'DynamicForm',
+  props: {
+    schema: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,9 +1,67 @@
 <template>
   <ArticleContent>
-    <div p="40" text="white">Article</div>
+    <div p="l-40 t-40" text="white">Article</div>
+    <VForm
+      ref="formRef"
+      v-slot="{ meta }"
+      @submit="test"
+      mt-10
+      w="1/2"
+      flex
+      flex-col
+      pl-40
+      text="white"
+    >
+      <VField name="email" type="email" rules="required|email" />
+      <ErrorMessage name="email" />
+      <VField name="password" type="password" rules="required|min:6" />
+      <ErrorMessage name="password" />
+      <button w="1/2" mx-auto mt-8 type="submit" :disabled="!meta.valid">
+        送出
+      </button>
+    </VForm>
+
+    <DynamicForm
+      :schema="formSchema"
+      mt-10
+      w="1/2"
+      flex
+      flex-col
+      pl-40
+      text="white"
+    />
   </ArticleContent>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import ArticleContent from '@/components/ArticleContent.vue';
+import DynamicForm from '@/components/DynamicForm.vue';
+
+const formRef = ref(null);
+
+const formSchema = ref({
+  fields: [
+    {
+      label: 'Your Name',
+      name: 'name',
+      as: 'input',
+    },
+    {
+      label: 'Your Email',
+      name: 'email',
+      as: 'input',
+    },
+    {
+      label: 'Your Password',
+      name: 'password',
+      as: 'input',
+    },
+  ],
+});
+
+const test = (value) => {
+  console.log('value', value);
+  formRef.value.resetForm();
+};
 </script>
